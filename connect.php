02d9fda746623d43a89a7e23b6fa7484230bb3f9
David Tereba <david@davidtereba.cz>
2013-10-23 16:25:09 +0200

    Aktuality

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?
-$db_server    = 'localhost'; // Server na kterém běží DB
-$db_login     = 'cztestnito'; // Login do DB
-$db_password  = 'r4kosn1cek'; // Heslo do DB
-$db_name      = 'cztestnito'; // Jméno DB
+$db_server    = ''; // Server na kterém běží DB
+$db_login     = ''; // Login do DB
+$db_password  = ''; // Heslo do DB
+$db_name      = ''; // Jméno DB
 $spojeni      = @MySQL_Connect($db_server ,$db_login, $db_password);
 @MySQL_Select_DB($db_name)or die('<p style="color: #CC0000">Nastala chyba v pripojeni k databazi');
 mysql_query("set names utf8");
